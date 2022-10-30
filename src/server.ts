a84876274eafda77424a6789ad7287f7384feda5
adriel <adrielarochaoronoz@gmail.com>
2022-10-30 10:53:42 +0000

    :card_file_box: adding basic configuration for mongodb :card_file_box:

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-import { createRestApiServer } from './core/servers'
+import { createRestApiServer, connectToDBServer, db } from './core/servers'
 import { envConstant } from './core/constants'
 
 const restApiServer = createRestApiServer()
@@ -7,8 +7,17 @@ const restApiServer = createRestApiServer()
 
 
 
-restApiServer.listen(envConstant.PORT, ()=>{
-    console.log(envConstant);
+restApiServer.listen(envConstant.PORT, async ()=>{
+    
+    
+    await connectToDBServer( envConstant.MONGODB_URI );
+    
+    const cards = await db.collection('cards').findOne();
+
+    console.log(cards);
+    
+
+    console.log(`The server is Ready in port ${envConstant.PORT}`);
 
     
 })
